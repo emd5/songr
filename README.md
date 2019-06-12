b69c296eaca61fba12b1292911f9aab7c0646249
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-06-12 11:15:16 -0700

    updated readme, wrote test and methods

--- README.md
+++ README.md
@@ -8,12 +8,40 @@ Liz Mahoney
 
 1.0.0
 
+## Getting Started
 
-## Feature
+1. Go to `https://start.spring.io/` to create a spring application.
 
-Ensure that you can run the Spring app.
-Create a hello world route at /hello, and ensure that you can visit that route in your browser and see data come back.
-Create a route that turns words into UPPER CASE. For instance, I should be able to visit /capitalize/hello and get back the response HELLO, or I could visit /capitalize/this is Kinda Capitalized and get the response THIS IS KINDA CAPITALIZED.
-Write a unit test for the capitalization functionality of that route.
-Create a route that reverses the order of the words in a query parameter. For example, if I visit /reverse?sentence=Hello world, it should return world Hello.
-Write a unit test for the word reversing functionality of the above route.
+
+![Spring application setup](assets/init.png)
+
+
+2. Open application in IntelliJ, select used import, and select appropriate JDK, click ok
+
+3. On terminal in root directory run `git init`
+3. Create a repo on github named `Songr`, then initial ACP to the Songr repo on github
+
+
+***To Run Application***
+
+In terminal run: `./gradlew bootrun`
+
+
+***To view gradle commands***
+
+In terminal run: `./gradlew tasks`
+
+***To Run Test***
+
+In terminal run: `./gradlew test`
+
+## Features
+
+### 6/12/19
+
+- [x] Ensure that you can run the Spring app.
+- [x] Create a hello world route at /hello, and ensure that you can visit that route in your browser and see data come back.
+- [x] Create a route that turns words into UPPER CASE. For instance, I should be able to visit /capitalize/hello and get back the response HELLO, or I could visit /capitalize/this is Kinda Capitalized and get the response THIS IS KINDA CAPITALIZED.
+- [x] Write a unit test for the capitalization functionality of that route.
+- [x] Create a route that reverses the order of the words in a query parameter. For example, if I visit /reverse?sentence=Hello world, it should return world Hello.
+- [x] Write a unit test for the word reversing functionality of the above route.
--- src/main/java/com/mahoneyliz/Java/Songr/SongrController.java
+++ src/main/java/com/mahoneyliz/Java/Songr/SongrController.java
@@ -1,19 +1,72 @@
 package com.mahoneyliz.Java.Songr;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@Controller
+/**
+ * The songr controller class
+ */
+@RestController
 public class SongrController {
 
+    /**
+     * Create a hello world route at /hello, and ensure that you can visit that route in your browser and see data
+     * come back.
+     *
+     * @return a welcome string
+     */
+    @GetMapping("/")
+    public String getRoot(){
+        return "Welcome to Songr";
+    }
+
+    /**
+     * Create a hello world route at /hello, and ensure that you can visit that route in your browser and see data
+     * come back.
+     * @return  a hello world string
+     */
     @GetMapping("/hello")
-    public String getHelloWorld(){
+    public String getHelloWorld() {
         return "Hello World!";
     }
 
+    /**
+     * Create a route that turns words into UPPER CASE. For instance, I should be able to visit /capitalize/hello
+     *and get back the response HELLO, or I could visit /capitalize/this is Kinda Capitalized and get the response
+     * THIS IS KINDA CAPITALIZED.
+     *
+     * @param word input string
+     * @return uppercase word
+     */
+    //
+    @RequestMapping(value = "/capitalize/{word}", method = GET)
+    @ResponseBody
     @GetMapping("/capitalize/{input}")
-    public String getRabbits(@PathVariable String input){
-        return input;
+    public String getCapitalize(@PathVariable("word") String word) {
+        return word.toUpperCase();
+    }
+
+    /**
+     * Create a route that reverses the order of the words in a query parameter. For example, if
+     * visit /reverse?sentence=Hello world, it should return world Hello.
+     *
+     * @param word input string
+     * @return reverse string
+     */
+    //
+    @RequestMapping(value = "/reverse", method = GET)
+    @ResponseBody
+    public String getReverse(@RequestParam("word") String word) {
+        String[] words = word.split(" ");
+        StringBuilder reverseSentence = new StringBuilder();
+
+        for(int i = words.length - 1; i >= 0 ; i--)
+        {
+            reverseSentence.append(words[i]);
+            reverseSentence.append(" ");
+        }
+        return String.valueOf(reverseSentence).trim();
     }
 }
+
+
